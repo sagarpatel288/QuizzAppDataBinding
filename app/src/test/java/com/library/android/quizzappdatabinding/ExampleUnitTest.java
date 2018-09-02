a7a352285a81cfb65da603f7ea6b317da59c6584
srdpatel <sagarpatel288@gmail.com>
2018-09-02 14:39:18 +0530

    First commit for first library project

--- app/src/test/java/com/library/android/quizzappdatabinding/ExampleUnitTest.java
+++ app/src/test/java/com/library/android/quizzappdatabinding/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.example.android.quizzappdatabinding;
+package com.library.android.quizzappdatabinding;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
